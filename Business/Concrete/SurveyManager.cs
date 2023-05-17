4fccea615e4d2ca92dd78c2048f50f64e2dfc040
Enes Doner <businessenesdoner@gmail.com>
2023-05-17 20:39:03 +0300

    Cashing yapıldı.

--- Business/Concrete/SurveyManager.cs
+++ Business/Concrete/SurveyManager.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Abstract;
 using Business.BusinessAspects.Autofac;
+using Core.Aspects.Autofac.Caching;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -13,7 +14,8 @@ namespace Business.Concrete
         {
             _surveydal = surveydal;
         }
-
+        
+        [CacheRemoveAspect("ISurveyService.Get")]
         public IResult Add(Survey survey)
         {
             _surveydal.Add(survey);
@@ -21,24 +23,25 @@ namespace Business.Concrete
         }
         
         [SecuredOperation("admin")]
-        [Cashei]
+        [CacheAspect(duration: 20)]
         public IDataResult<List<Survey>> GetAll()
         {
             return new SuccessDataResult<List<Survey>>(_surveydal.GetAll());
         }
-
+      
+        [CacheRemoveAspect("ISurveyService.Get")]
         public IResult Delete(Survey survey)
         {
             _surveydal.Delete(survey);
             return new SuccessResult();
         }
-
+        [CacheRemoveAspect("ISurveyService.Get")]
         public IResult Update(Survey survey)
         {
            _surveydal.Update(survey);
             return new SuccessResult();
         }
-
+        [CacheAspect(duration: 2)]
         public IDataResult<Survey> GetById(string Id)
         {
             return new SuccessDataResult<Survey>(_surveydal.Get(s => s.Id == Id));
